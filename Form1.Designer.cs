ffc69b660b97b25a1e2b3b80efb627f6ee17d31b
Rui Pinto <rui.s.pinto@hotmail.com>
2021-03-01 11:56:20 +0000

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace ChessBoardGUIApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(505, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select a piece from the drop down menu and I\'ll show your legal moves";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Bishop",
+            "Rook",
+            "Knight",
+            "King",
+            "Queen",
+            "Pawn"});
+            this.comboBox1.Location = new System.Drawing.Point(524, 5);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(176, 28);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 45);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(500, 500);
+            this.panel1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(572, 497);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 39);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "&Exit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(712, 553);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Chess Piece Moves";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,121 @@
+﻿using ChessBoardModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChessBoardGUIApp
+{
+    public partial class Form1 : Form
+    {
+        static Board myBoard = new Board(8);
+
+        public Button[,] btnGrid = new Button[myBoard.Size, myBoard.Size];
+
+        public Form1()
+        {
+            InitializeComponent();
+            populateGrid();
+        }
+
+        private void populateGrid()
+        {
+            // create buttons and place them into panel1
+            int buttonSize = panel1.Width / 8;
+
+
+            // make the panel a perfect square
+            panel1.Height = panel1.Height;
+
+
+            // nested loops. create buttons and print them to the screen
+            for (int i = 0; i < myBoard.Size; i++)
+            {
+                for (int j = 0; j < myBoard.Size; j++)
+                {
+                    btnGrid[i, j] = new Button();
+                    btnGrid[i, j].Height = buttonSize;
+                    btnGrid[i, j].Width = buttonSize;
+                    btnGrid[i, j].Font = new Font("Microsoft Sans Serif", 12);
+
+                    // add a click event to each button
+                    btnGrid[i, j].Click += Grid_Button_Click;
+
+                    // add the new button to panel1
+                    panel1.Controls.Add(btnGrid[i, j]);
+
+                    // set the location of each new button
+                    btnGrid[i, j].Location = new Point(i * buttonSize, j * buttonSize);
+
+                    btnGrid[i, j].Text = i + " | " + j;
+                    btnGrid[i, j].Tag = new Point(i, j);
+
+                }
+            }
+        }
+
+        private void Grid_Button_Click(object sender, EventArgs e)
+        {
+            // get row and column number of the button clicked
+            Button clickedButton = (Button)sender;
+            Point location = (Point)clickedButton.Tag;
+
+            int x = location.X;
+            int y = location.Y;
+
+            //int option = 0;
+
+            Cell currentCell = myBoard.theGrid[x, y];
+
+            /*
+            // depending the option chosen it 'translates' to int
+            if (comboBox1.SelectedText == "Knight")
+                option = 1;
+            else if (comboBox1.SelectedText == "King")
+                option = 2;
+            else if (comboBox1.SelectedText == "Rook")
+                option = 3;
+            else if (comboBox1.SelectedText == "Bishop")
+                option = 4;
+            else if (comboBox1.SelectedText == "Queen")
+                option = 5;
+            else
+                option = 6;
+            */
+
+            // determine legal next moves
+            myBoard.MarkNextLegalMoves(currentCell, comboBox1.SelectedIndex);
+
+            // update the text on each button
+            for (int i = 0; i < myBoard.Size; i++)
+            {
+                for (int j = 0; j < myBoard.Size; j++)
+                {
+                    btnGrid[i, j].Text = "";
+                    if (myBoard.theGrid[i, j].LegalNexMove == true)
+                    {
+                        btnGrid[i, j].Text = "Legal";
+                    }
+                    else if (myBoard.theGrid[i, j].CurrentlyOccupied == true)
+                    {
+                        //btnGrid[i, j].Text = comboBox1.SelectedItem.ToString();
+                        btnGrid[i, j].Text = "Knight";
+
+                    }
+                }
+            }
+
+            // MessageBox.Show("You clicked the button " + x + "|" + y);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
